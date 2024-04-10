56f2a6d425276f1f93cdb6e9c5578b020f6b6c32
Navinkumar Boddu <swarmrobots@gmail.com>
2024-04-10 19:21:43 +0530

    Closes #8 added TeeingCollector solution

--- src/main/java/org/java9/TeeingCollector.java
+++ src/main/java/org/java9/TeeingCollector.java
@@ -5,10 +5,15 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Comparator;
+import java.util.IntSummaryStatistics;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.maxBy;
+import static java.util.stream.Collectors.minBy;
 
 public class TeeingCollector {
 
@@ -22,6 +27,13 @@ public class TeeingCollector {
         System.out.println("Solution 1");
         readAndStreamTwice(path);
 
+        //Solution 2
+        System.out.println("Solution 2");
+        streamTwice(path);
+
+        //Solution 3
+        System.out.println("Solution 3");
+        teeingCollector(path);
     }
 
     private static void readAndStreamTwice(Path path) throws IOException {
@@ -34,5 +46,51 @@ public class TeeingCollector {
             Optional<String> max = line.max(comparing(String::length));
             System.out.println("Longest word : " + max.orElseGet(() -> "Word not found"));
         }
+    }
+
+    private static void streamTwice(Path path) throws IOException {
+
+        try(Stream<String> line = Files.lines(path)){
+            Optional<String> min = line.min(comparing(String::length));
+            //Optional<String> max = line.max(comparing(String::length));
+            //stream has already been operated upon or closed
+
+            System.out.println("Smallest word : " + min.orElseGet(() -> "Word not found"));
+            //System.out.println("Longest word : " + max.orElseGet(() -> "Word not found"));
+        }
+    }
+
+    private static void teeingCollector(Path path) throws IOException {
+
+        try(Stream<String> line = Files.lines(path)){
+            Statistics data = line.collect(Collectors.teeing(
+                    minBy(comparing(String::length)),
+                    maxBy(comparing(String::length)),
+                    (smallestWord, longestWord) -> new Statistics(smallestWord.orElse(null), longestWord.orElse(null))
+            ));
+
+            System.out.println(data);
+        }
+
+
+    }
+}
+
+class Statistics {
+
+    final private String smallestWord;
+    final private String longestWord;
+
+    public Statistics(String smallestWord, String longestWord) {
+        this.smallestWord = smallestWord;
+        this.longestWord = longestWord;
+    }
+
+    @Override
+    public String toString() {
+        return "Statistics{" +
+                "smallestWord='" + smallestWord + '\'' +
+                ", longestWord='" + longestWord + '\'' +
+                '}';
     }
 }
