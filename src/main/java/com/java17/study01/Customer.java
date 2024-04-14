2ee355df654ea90ec130bb86ba2904ada6ba6021
Navinkumar Boddu <swarmrobots@gmail.com>
2024-04-14 22:10:09 +0530

    Closes #16 Added Override Accessor methods example for Records

---
+++ src/main/java/com/java17/study01/Customer.java
@@ -0,0 +1,13 @@
+package com.java17.study01;
+
+public record Customer(long id, String name, String email) {
+
+    @Override
+    public String name(){
+        return  name!=null && !name.isBlank() ? name : "anonymous";
+    }
+
+    //Rules:
+    // - The return type of an overridden accessor method must be the same as the type of the component
+    // - Accessor methods cannot have a 'throws' clause
+}
---
+++ src/main/java/com/java17/study01/RecordExample2.java
@@ -0,0 +1,15 @@
+package com.java17.study01;
+
+public class RecordExample2 {
+
+    public static void main(String[] args) {
+        var customer = new Customer(1001L,null,null);
+        System.out.println(customer.name());
+
+        var customerCopy = new Customer((customer.id()), customer.name(), customer.email());
+        System.out.println(customerCopy.equals(customer));
+
+        System.out.println(customer);
+        System.out.println(customerCopy);
+    }
+}
